d64784f247cedaff0a7bc2eaaaf8c16ec7f851ae
mamanaby3 <a.drame@innovimpactafrica.com>
2026-01-06 13:36:17 +0000

    ssl

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,5 @@
 import { Pool } from "pg";
-export const db = new Pool({ connectionString: process.env.DATABASE_URL,ssl: { rejectUnauthorized: false }  });
+export const db = new Pool({ connectionString: process.env.DATABASE_URL ,ssl: false,   });
 
 export async function query(text: string, params?: any[]) {
     const res = await db.query(text, params);
