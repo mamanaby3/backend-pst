d37f6d0981ff04427a6be8df4a4ad6f7322d1e2d
mamanaby3 <a.drame@innovimpactafrica.com>
2026-01-06 12:00:53 +0000

    modification de db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,5 @@
 import { Pool } from "pg";
-export const db = new Pool({ connectionString: process.env.DATABASE_URL });
+export const db = new Pool({ connectionString: process.env.DATABASE_URL,ssl: { rejectUnauthorized: false }  });
 
 export async function query(text: string, params?: any[]) {
     const res = await db.query(text, params);
